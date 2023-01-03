98fb321b1e6f85e1abcc5ef188897f98d7b92d21
autumnchris <autumncbullard@gmail.com>
2023-01-03 12:55:29 -0500

    Use PascalCase on component file names

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { json } from 'd3';
-import BarChart from './Bar-Chart';
-import ErrorMessage from './Error-Message';
-import LoadingSpinner from './Loading-Spinner';
+import BarChart from './BarChart';
+import ErrorMessage from './ErrorMessage';
+import LoadingSpinner from './LoadingSpinner';
 
 const App = () => {
   const [loadingStatus, setLoadingStatus] = useState(true);
