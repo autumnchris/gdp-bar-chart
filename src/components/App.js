2c23c80eeb6e820cb59cba55ac5b4d27f6ae9aa5
autumnchris <autumncbullard@gmail.com>
2023-01-02 20:49:18 -0500

    Move tooltip functionality to Bar-Chart component

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { json, select } from 'd3';
+import { json } from 'd3';
 import BarChart from './Bar-Chart';
 import ErrorMessage from './Error-Message';
 import LoadingSpinner from './Loading-Spinner';
-import Tooltip from './Tooltip';
 
 const App = () => {
   const [loadingStatus, setLoadingStatus] = useState(true);
   const [loadSuccess, setLoadSuccess] = useState(false);
   const [gdpData, setGDPData] = useState([]);
-  const [tooltip, setTooltip] = useState(null);
 
   useEffect(() => {
     json('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json').then(dataset => {
@@ -24,25 +22,6 @@ const App = () => {
     });
   }, []);
 
-  function handleMouseEnter(event, value) {
-    setTooltip({
-      date: value[0],
-      amount: value[1],
-      left: `${(event.pageX - 50)}px`,
-      top: `${(event.pageY - 80)}px`
-    });
-
-    select(event.currentTarget)
-      .attr('fill', 'hsl(0, 0%, 20%)');
-  }
-
-  function handleMouseLeave(event) {
-    setTooltip(null);
-
-    select(event.currentTarget)
-      .attr('fill', '#4ddbff');
-  }
-
   return (
     <React.Fragment>
       <header>
@@ -50,8 +29,7 @@ const App = () => {
         <h2>1947-2015</h2>
       </header>
       <main>
-        {loadingStatus ? <LoadingSpinner /> : loadSuccess ? <BarChart gdpData={gdpData} handleMouseEnter={handleMouseEnter} handleMouseLeave={handleMouseLeave} /> : <ErrorMessage />}
-        {tooltip && <Tooltip tooltip={tooltip} />}
+        {loadingStatus ? <LoadingSpinner /> : loadSuccess ? <BarChart gdpData={gdpData} /> : <ErrorMessage />}
       </main>
       <footer>Created by <a href="https://autumnchris.github.io/portfolio" target="_blank">Autumn Bullard</a> &copy; {new Date().getFullYear()}</footer>
     </React.Fragment>
--- src/components/Bar-Chart.js
+++ src/components/Bar-Chart.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import { scaleTime, scaleLinear, extent, max } from 'd3';
+import React, { useState } from 'react';
+import { select, scaleTime, scaleLinear, extent, max } from 'd3';
 import Axis from './Axis';
 import Bar from './Bar';
+import Tooltip from './Tooltip';
 
-const BarChart = ({ gdpData, handleMouseEnter, handleMouseLeave }) => {
+const BarChart = ({ gdpData }) => {
   const margin = {
     top: 50,
     right: 40,
@@ -18,6 +19,27 @@ const BarChart = ({ gdpData, handleMouseEnter, handleMouseLeave }) => {
   const yScale = scaleLinear()
     .domain([0, max(gdpData, (d) => d[1])])
     .range([h - margin.top, margin.bottom]);
+  
+  const [tooltip, setTooltip] = useState(null);
+
+  function handleMouseEnter(event, value) {
+    setTooltip({
+      date: value[0],
+      amount: value[1],
+      left: `${(event.pageX - 50)}px`,
+      top: `${(event.pageY - 80)}px`
+    });
+
+    select(event.currentTarget)
+      .attr('fill', 'hsl(0, 0%, 20%)');
+  }
+  
+  function handleMouseLeave(event) {
+    setTooltip(null);
+
+    select(event.currentTarget)
+      .attr('fill', '#4ddbff');
+  }
 
   return (
     <div className="chart-container">
@@ -27,6 +49,7 @@ const BarChart = ({ gdpData, handleMouseEnter, handleMouseLeave }) => {
         <text className="y-label" x="-260" y="40" transform="rotate(-90)">Gross Domestic Product in Billions</text>
         {gdpData.map((bar, i) => <Bar key={i} bar={bar} x={xScale(new Date(bar[0]))} y={yScale(bar[1])} width={w / gdpData.length} height={h - yScale(bar[1]) - margin.top} handleMouseEnter={handleMouseEnter} handleMouseLeave={handleMouseLeave} />)}
       </svg>
+      {tooltip && <Tooltip tooltip={tooltip} />}
     </div>
   );
 }
